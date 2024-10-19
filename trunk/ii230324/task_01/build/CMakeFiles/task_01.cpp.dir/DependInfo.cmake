9e37596bad5462dba2d9d46df98d0425b30e7d76
anastasecher <anastasiachernoivan@gmail.com>
2024-10-19 12:00:06 +0300

    .

---
+++ trunk/ii230324/task_01/build/CMakeFiles/task_01.cpp.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/anastasiacernoivan/Documents/уник/2 курс/отис/OTIS-2024/trunk/ii02612/task_01/src/task_01.cpp" "CMakeFiles/task_01.cpp.dir/task_01.cpp.o" "gcc" "CMakeFiles/task_01.cpp.dir/task_01.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
