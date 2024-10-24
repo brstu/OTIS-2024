2c0ad82454540dcb3f291305665d84b8d77de1ba
Глеб Горбат <proboy4ic@gmail.com>
2024-10-24 12:26:15 +0300

    fix main

--- trunk/ii230278/task_02/src/main.cpp
+++ trunk/ii230278/task_02/src/main.cpp
@@ -1,32 +1,7 @@
 #include <iostream>
 #include <vector>
-
+using namespace std;
 int main() {
-    double YS = 0.0; // Инициализация YS
-    double W = 10.0; // Инициализация W
-
-    // Теперь можно инициализировать векторы
-    std::vector<double> y;
-    y.push_back(YS);
-    double U = 1.0;
-    std::vector<double> E;
-    E.push_back(W - YS);
-    std::vector<double> um;
-    um.push_back(U);
-
-    // Вывод для проверки
-    std::cout << "y: ";
-    for (const auto& val : y) {
-        std::cout << val << " ";
-    }
-    std::cout << "nE: ";
-    for (const auto& val : E) {
-        std::cout << val << " ";
-    }
-    std::cout << "num: ";
-    for (const auto& val : um) {
-        std::cout << val << " ";
-    }
-
+   cout << "Hello world";
     return 0;
 }
