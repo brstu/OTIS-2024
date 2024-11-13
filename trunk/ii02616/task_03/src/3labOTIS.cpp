16c6ce2c3811ee4c993004ab346d83b0e2e81e96
ArsenikDev <popapisa2006@mail.ru>
2024-11-13 19:32:19 +0400

    Change code file and add mark

--- trunk/ii02616/task_03/src/3labOTIS.cpp
+++ trunk/ii02616/task_03/src/3labOTIS.cpp
@@ -1,17 +1,17 @@
-#include <string>
+#include <iostream>
 #include <unordered_map>
-#include <vector>
+#include <unordered_set>
+#include <list>
 #include <fstream>
-#include <limits>
+#include <sstream>
 #include <iomanip>
+#include <limits>
 
 class Node {
 public:
     std::string name;
-    std::string content;  // Текстовое содержимое узла
-    std::string fileLink; // Ссылка на файл
+    std::unordered_set<std::string> connectedNodes;  // Множество смежных узлов
 
-    Node() = default;
     Node(const std::string& nodeName) : name(nodeName) {}
 };
 
@@ -21,74 +21,84 @@ public:
     std::string to;
     bool directed;
 
-    Edge(const std::string& from, const std::string& to, bool directed = false)
-        : from(from), to(to), directed(directed) {}
+    Edge(const std::string& fromNode, const std::string& toNode, bool isDirected = false)
+        : from(fromNode), to(toNode), directed(isDirected) {}
 };
 
 class Graph {
-public:
+private:
     std::string name;
-    bool isDirected;
+    bool directed;
     std::unordered_map<std::string, Node> nodes;
-    std::vector<Edge> edges;
+    std::list<Edge> edges;
 
-    Graph(const std::string& graphName, bool directed)
-        : name(graphName), isDirected(directed) {}
+public:
+    Graph(const std::string& graphName, bool isDirected)
+        : name(graphName), directed(isDirected) {}
 
     void addNode(const std::string& nodeName) {
         if (nodes.find(nodeName) == nodes.end()) {
             nodes[nodeName] = Node(nodeName);
-            std::cout << "Node " << nodeName << " added.\n";
-        }
-        else {
-            std::cout << "Node " << nodeName << " already exists.\n";
+            std::cout << "Node '" << nodeName << "' successfully added.\n";
+        } else {
+            std::cout << "Node '" << nodeName << "' already exists.\n";
         }
     }
 
     void removeNode(const std::string& nodeName) {
-        if (nodes.erase(nodeName)) {
-            edges.erase(std::remove_if(edges.begin(), edges.end(), [&](Edge& edge) {
+        auto it = nodes.find(nodeName);
+        if (it != nodes.end()) {
+            nodes.erase(it);
+            // Удаляем все рёбра, связанные с этим узлом
+            edges.remove_if([&](const Edge& edge) {
                 return edge.from == nodeName || edge.to == nodeName;
-                }), edges.end());
-            std::cout << "Node " << nodeName << " removed.\n";
-        }
-        else {
-            std::cout << "Node " << nodeName << " does not exist.\n";
+            });
+            std::cout << "Node '" << nodeName << "' has been removed.\n";
+        } else {
+            std::cout << "Node '" << nodeName << "' does not exist.\n";
         }
     }
 
     void addEdge(const std::string& from, const std::string& to) {
-        if (nodes.find(from) != nodes.end() && nodes.find(to) != nodes.end()) {
-            edges.emplace_back(from, to, isDirected);
-            std::cout << "Edge from " << from << " to " << to << " added.\n";
-        }
-        else {
+        if (nodes.count(from) && nodes.count(to)) {
+            edges.push_back(Edge(from, to, directed));
+            nodes[from].connectedNodes.insert(to);
+            if (!directed) {
+                nodes[to].connectedNodes.insert(from);  // Для неориентированного графа добавляем обратное ребро
+            }
+            std::cout << "Edge from '" << from << "' to '" << to << "' successfully added.\n";
+        } else {
             std::cout << "One or both nodes do not exist.\n";
         }
     }
 
     void removeEdge(const std::string& from, const std::string& to) {
-        edges.erase(std::remove_if(edges.begin(), edges.end(), [&](Edge& edge) {
-            return edge.from == from && edge.to == to;
-            }), edges.end());
-        std::cout << "Edge from " << from << " to " << to << " removed.\n";
+        edges.remove_if([&](const Edge& edge) {
+            return (edge.from == from && edge.to == to);
+        });
+        nodes[from].connectedNodes.erase(to);
+        if (!directed) {
+            nodes[to].connectedNodes.erase(from);
+        }
+        std::cout << "Edge from '" << from << "' to '" << to << "' has been removed.\n";
     }
 
-    void exportGraph(const std::string& filename) {
+    void exportGraph(const std::string& filename) const {
         std::ofstream outFile(filename);
         if (outFile.is_open()) {
-            outFile << name << " : " << (isDirected ? "ORIENT" : "UNORIENT") << " ;\n";
+            outFile << name << " : " << (directed ? "ORIENT" : "UNORIENT") << " ;\n";
+            outFile << "Nodes: ";
             for (const auto& nodePair : nodes) {
                 outFile << nodePair.first << " ";
             }
             outFile << ";\n";
+
             for (const auto& edge : edges) {
-                outFile << edge.from << " -> " << edge.to << (isDirected ? " ;" : " ");
+                outFile << edge.from << " -> " << edge.to << (directed ? " ;" : " ");
             }
             outFile.close();
-            std::cout << "Graph exported to " << filename << "\n";
-        }
-        else {
+            std::cout << "Graph successfully exported to '" << filename << "'\n";
+        } else {
             std::cout << "Error opening file for export.\n";
         }
     }
@@ -99,53 +109,52 @@ public:
             nodes.clear();
             edges.clear();
             std::string line;
-            getline(inFile, line);
-            size_t pos = line.find(" : ");
-            name = line.substr(0, pos);
-            isDirected = line.find("ORIENT") != std::string::npos;
 
-            getline(inFile, line);
-            pos = 0;
-            while ((pos = line.find(',')) != std::string::npos) {
-                std::string nodeName = line.substr(0, pos);
+            // Read the first line for graph info
+            std::getline(inFile, line);
+            std::istringstream graphInfo(line);
+            std::string graphType;
+            graphInfo >> name >> graphType;
+            directed = graphType == "ORIENT";
+
+            // Read the node list
+            std::getline(inFile, line);
+            std::istringstream nodeStream(line);
+            std::string nodeName;
+            while (nodeStream >> nodeName) {
                 addNode(nodeName);
-                line.erase(0, pos + 1);
             }
-            if (!line.empty()) addNode(line);
 
-            getline(inFile, line);
-            while ((pos = line.find("->")) != std::string::npos) {
-                std::string from = line.substr(0, pos);
-                line.erase(0, pos + 2);
-                pos = line.find(',');
-                std::string to = line.substr(0, pos);
+            // Read the edge list
+            std::getline(inFile, line);
+            std::istringstream edgeStream(line);
+            std::string from, to;
+            while (edgeStream >> from >> to) {
                 addEdge(from, to);
-                if (pos == std::string::npos) break;
-                line.erase(0, pos + 1);
             }
+
             inFile.close();
-            std::cout << "Graph imported from " << filename << "\n";
-        }
-        else {
+            std::cout << "Graph successfully imported from '" << filename << "'\n";
+        } else {
             std::cout << "Error opening file for import.\n";
         }
     }
 
-    void displayInfo() {
+    void displayInfo() const {
         std::cout << "Graph: " << name << "\n";
         std::cout << "Number of nodes: " << nodes.size() << "\n";
         std::cout << "Number of edges: " << edges.size() << "\n";
     }
 
-    void drawGraph() {
-        std::cout << "\n--- ASCII Graph Representation ---\n";
+    void drawGraph() const {
+        std::cout << "\n--- Graph Visualization ---\n";
         for (const auto& edge : edges) {
-            std::cout << std::setw(2) << edge.from << " --> " << edge.to << "\n";
-            if (!isDirected) {
-                std::cout << std::setw(2) << edge.to << " --> " << edge.from << "\n";
+            std::cout << edge.from << " --> " << edge.to << "\n";
+            if (!directed) {
+                std::cout << edge.to << " --> " << edge.from << "\n";
             }
         }
-        std::cout << "----------------------------------\n";
+        std::cout << "----------------------------\n";
     }
 };
 
@@ -164,14 +173,14 @@ void displayMenu() {
 }
 
 int main() {
-    Graph graph("ExampleGraph", true);
+    Graph graph("ExampleGraph", true);  // Example directed graph
     int choice;
     std::string nodeName, fromNode, toNode, filename;
 
     while (true) {
         displayMenu();
         std::cin >> choice;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Очистка буфера ввода
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Clear input buffer
 
         switch (choice) {
         case 1:
