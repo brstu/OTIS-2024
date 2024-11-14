2bcc08504a2e37b195d66d3bbf257b6235258a66
миша <rulko.mkes@gmail.com>
2024-11-14 19:46:43 +0300

    task_01

--- trunk/ii002615/task_01/src/Source1.cpp
+++ trunk/ii002615/task_01/src/Source1.cpp
@@ -1,60 +1,55 @@
-#include<iostream>
-#include<cmath>
-
-
-
-double coeff_a = 1.0,
-b = 0.5,
-c = 0.1,
-d = 0.9,
-U = 1.5;
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void LINEYKA(double& TLEN);
-void nonLINEYKA(double& temp_nonlinear, double& prev_temp_nonlinear, bool& is_first_iteration);
+const double коэффициент_a = 1.0;
+const double коэффициент_b = 0.5;
+const double коэффициент_c = 0.1;
+const double коэффициент_d = 0.9;
+const double U = 1.5;
 
+void процессЛинейка(double& длина, int итерации);
+void процессНеЛинейка(double& температура, int итерации);
 
 int main() {
-	setlocale(LC_ALL, "rus");
-	double TLEN = 0, temp_nonlinear = 0, prev_temp_nonlinear = 0;
-	bool is_first_iteration = true; // используется для определения первой итерации в функции нелинейной модели 
-	cout << "какая начальная температура?";
-	cin >> TLEN;
-	temp_nonlinear = TLEN;
+    setlocale(LC_ALL, "rus");
+    double начальнаяТемпература = 0;
 
+    cout << "какая начальная температура? ";
+    cin >> начальнаяТемпература;
 
-	int iterations = 10;
-	cout << "линейная модель:" << endl;
-	for (int i = 0; i < iterations; i++) {
-		LINEYKA(TLEN);
-	}
+    // Процесс линейной модели
+    процессЛинейка(начальнаяТемпература, 10);
 
-	cout << "нелинейная модель:" << endl;
-	for (int i = 0; i < iterations; i++) {
-		nonLINEYKA(temp_nonlinear, prev_temp_nonlinear, is_first_iteration);
-	}
+    // Процесс нелинейной модели
+    процессНеЛинейка(начальнаяТемпература, 10);
 
-	return 0;
+    return 0;
 }
-void LINEYKA(double& TLEN)
-{
-	TLEN = coeff_a * TLEN + b * U;
-	cout << "////" << TLEN <<  "////" << endl;
+
+void процессЛинейка(double& длина, int итерации) {
+    cout << "линейная модель:" << endl;
+    for (int i = 0; i < итерации; i++) {
+        длина = коэффициент_a * длина + коэффициент_b * U;
+        cout << "////" << длина << "////" << endl;
+    }
 }
-void nonLINEYKA(double& temp_nonlinear, double& prev_temp_nonlinear, bool& is_first_iteration)
-{
-	if (is_first_iteration) {
-		prev_temp_nonlinear = temp_nonlinear;
-		temp_nonlinear = coeff_a * temp_nonlinear + c * U + d * sin(U);
-		is_first_iteration = false; // первая итерация закончена
-		cout << "////"   << temp_nonlinear << "////" << endl;
-	}
-	else {
-		double temp_new = 0; // переменная для хранения значения новой температуры 
-		temp_new = coeff_a * temp_nonlinear - b * pow(prev_temp_nonlinear, 2) + c * U + d * sin(U);
-		prev_temp_nonlinear = temp_nonlinear; // устанавливаем новое значение для предыдущего значения
-		temp_nonlinear = temp_new; // устанавливаем новое значение для текущего значения
-		cout << "////"  << temp_nonlinear << "////" << endl;
-	}
+
+void процессНеЛинейка(double& температура, int итерации) {
+    double предыдущаяТемпература = температура;
+    bool перваяИтерация = true;
+
+    cout << "нелинейная модель:" << endl;
+    for (int i = 0; i < итерации; i++) {
+        if (перваяИтерация) {
+            температура = коэффициент_a * температура + коэффициент_c * U + коэффициент_d * sin(U);
+            перваяИтерация = false;
+        } else {
+            double новаяТемпература = коэффициент_a * температура - коэффициент_b * pow(предыдущаяТемпература, 2) + коэффициент_c * U + коэффициент_d * sin(U);
+            предыдущаяТемпература = температура;
+            температура = новаяТемпература;
+        }
+        cout << "////" << температура << "////" << endl;
+    }
 }
