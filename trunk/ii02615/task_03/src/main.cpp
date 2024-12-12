0aadb764fb4f9adc770bec551d5e7cc1909893ac
миша <rulko.mkes@gmail.com>
2024-12-12 22:07:22 +0300

    fix code 3

--- trunk/ii02615/task_03/src/main.cpp
+++ trunk/ii02615/task_03/src/main.cpp
@@ -5,144 +5,154 @@
 
 using namespace std;
 
-class Graph {
+class AbC {  // Изменено название класса
 private:
-    struct Node {
-        int id;
-        list<int> neighbors;
+    struct XyZ {  // Изменено название структуры
+        int a;
+        list<int> b;
     };
 
-    vector<Node> nodes;
-
-    bool hasEvenDegree() const {
-        for (const auto& node : nodes) {
-            if (node.neighbors.size() % 2 != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool findHamiltonianCycle(int currentNode, int depth, vector<int>& cycle, vector<bool>& visited) {
-        if (depth == nodes.size()) {
-            return cycle.front() == cycle.back();
-        }
-
-        for (int neighbor : nodes[currentNode].neighbors) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
-                cycle[depth] = neighbor;
-                if (findHamiltonianCycle(neighbor, depth + 1, cycle, visited)) {
-                    return true;
-                }
-                visited[neighbor] = false;
-            }
-        }
-        return false;
-    }
+    vector<XyZ> c;
 
 public:
-    void addNode(int id) {
-        nodes.push_back({ id, {} });
-    }
+    void dE(int f);
+    void gH(int i, int j);
+    void iJ() const;
+    vector<int> kL();
+    vector<int> mN();
+    AbC oP() const;
 
-    void addEdge(int start, int end) {
-        nodes[start].neighbors.push_back(end);
-        nodes[end].neighbors.push_back(start);
-    }
+    bool qR() const;
+    bool sT(int u, int v, vector<int>& w, vector<bool>& x);
+};
 
-    void displayGraph() const {
-        for (const auto& node : nodes) {
-            cout << "Node " << node.id << ": ";
-            for (int neighbor : node.neighbors) {
-                cout << neighbor << " ";
-            }
-            cout << endl;
+void AbC::dE(int f) {
+    c.push_back({ f, {} });
+}
+
+void AbC::gH(int i, int j) {
+    c[i].b.push_back(j);
+    c[j].b.push_back(i);
+}
+
+void AbC::iJ() const {
+    for (const auto& node : c) {
+        cout << "Node " << node.a << ": ";
+        for (int neighbor : node.b) {
+            cout << neighbor << " ";
+        }
+        cout << endl;
+    }
+}
+
+vector<int> AbC::kL() {
+    vector<int> cycle;
+    if (!qR()) return cycle;
+
+    vector<bool> visited(c.size(), false);
+    list<int> stack;
+    stack.push_back(0);
+
+    while (!stack.empty()) {
+        int current = stack.back();
+        if (!c[current].b.empty()) {
+            int next = c[current].b.front();
+            stack.push_back(next);
+            c[current].b.remove(next);
+            c[next].b.remove(current);
+        } else {
+            cycle.push_back(current);
+            stack.pop_back();
         }
     }
+    return cycle;
+}
 
-    vector<int> getEulerianCycle() {
-        vector<int> cycle;
-        if (!hasEvenDegree()) return cycle;
+vector<int> AbC::mN() {
+    vector<int> cycle(c.size(), -1);
+    vector<bool> visited(c.size(), false);
+    visited[0] = true;
+    cycle[0] = 0;
 
-        vector<bool> visited(nodes.size(), false);
-        list<int> stack;
-        stack.push_back(0);
-
-        while (!stack.empty()) {
-            int currentNode = stack.back();
-            if (!nodes[currentNode].neighbors.empty()) {
-                int nextNeighbor = nodes[currentNode].neighbors.front();
-                stack.push_back(nextNeighbor);
-                nodes[currentNode].neighbors.remove(nextNeighbor);
-                nodes[nextNeighbor].neighbors.remove(currentNode);
-            } else {
-                cycle.push_back(currentNode);
-                stack.pop_back();
-            }
-        }
+    if (sT(0, 1, cycle, visited)) {
         return cycle;
     }
+    return {};
+}
 
-    vector<int> getHamiltonianCycle() {
-        vector<int> cycle(nodes.size(), -1);
-        vector<bool> visited(nodes.size(), false);
-        visited[0] = true;
-        cycle[0] = 0;
-
-        if (findHamiltonianCycle(0, 1, cycle, visited)) {
-            return cycle;
-        }
-        return {};
+AbC AbC::oP() const {
+    AbC tree;
+    for (int i = 0; i < c.size(); ++i) {
+        tree.dE(i);
     }
 
-    Graph getSpanningTree() const {
-        Graph spanningTree;
-        for (int i = 0; i < nodes.size(); ++i) {
-            spanningTree.addNode(i);
-        }
+    vector<bool> visited(c.size(), false);
+    visited[0] = true;
+    queue<int> q;
+    q.push(0);
 
-        vector<bool> visited(nodes.size(), false);
-        visited[0] = true;
-        queue<int> queue;
-        queue.push(0);
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
 
-        while (!queue.empty()) {
-            int currentNode = queue.front();
-            queue.pop();
-
-            for (int neighbor : nodes[currentNode].neighbors) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    spanningTree.addEdge(currentNode, neighbor);
-                    queue.push(neighbor);
-                }
+        for (int neighbor : c[current].b) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                tree.gH(current, neighbor);
+                q.push(neighbor);
             }
         }
-        return spanningTree;
     }
-};
+    return tree;
+}
+
+bool AbC::qR() const {
+    for (const auto& node : c) {
+        if (node.b.size() % 2 != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool AbC::sT(int u, int v, vector<int>& cycle, vector<bool>& visited) {
+    if (v == c.size()) {
+        return cycle.front() == cycle.back();
+    }
+
+    for (int neighbor : c[u].b) {
+        if (!visited[neighbor]) {
+            visited[neighbor] = true;
+            cycle[v] = neighbor;
+            if (sT(neighbor, v + 1, cycle, visited)) {
+                return true;
+            }
+            visited[neighbor] = false;
+        }
+    }
+    return false;
+}
 
 int main() {
     setlocale(LC_ALL, "RUSSIAN");
-    Graph graph;
+    AbC xyz;  // Изменено название объекта класса
 
-    graph.addNode(0);
-    graph.addNode(1);
-    graph.addNode(2);
-    graph.addNode(3);
-    graph.addNode(4);
+    xyz.dE(0);
+    xyz.dE(1);
+    xyz.dE(2);
+    xyz.dE(3);
+    xyz.dE(4);
 
-    graph.addEdge(0, 1);
-    graph.addEdge(1, 2);
-    graph.addEdge(2, 3);
-    graph.addEdge(3, 4);
-    graph.addEdge(4, 0);
+    xyz.gH(0, 1);
+    xyz.gH(1, 2);
+    xyz.gH(2, 3);
+    xyz.gH(3, 4);
+    xyz.gH(4, 0);
 
     cout << "Структура графа:\n";
-    graph.displayGraph();
+    xyz.iJ();
 
-    vector<int> eulerCycle = graph.getEulerianCycle();
+    vector<int> eulerCycle = xyz.kL();
     if (!eulerCycle.empty()) {
         cout << "Эйлеров цикл: ";
         for (int node : eulerCycle) {
@@ -153,7 +163,7 @@ int main() {
         cout << "Эйлеров цикл не найден.\n";
     }
 
-    vector<int> hamiltonianCycle = graph.getHamiltonianCycle();
+    vector<int> hamiltonianCycle = xyz.mN();
     if (!hamiltonianCycle.empty()) {
         cout << "Гамильтонов цикл: ";
         for (int node : hamiltonianCycle) {
@@ -164,9 +174,9 @@ int main() {
         cout << "Гамильтонов цикл не найден.\n";
     }
 
-    Graph spanningTree = graph.getSpanningTree();
+    AbC spanningTree = xyz.oP();  // Изменено название объекта класса
     cout << "Создание структуры дерева:\n";
-    spanningTree.displayGraph();
+    spanningTree.iJ();
 
     return 0;
 }
