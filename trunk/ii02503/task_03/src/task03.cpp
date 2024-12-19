bffae112d7a6504545d592cc0a9ec1001b2d54dd
AndreyukMax <andreyukmax@gmail.com>
2024-12-19 13:25:19 +0300

    fixed cpp file

--- trunk/ii02503/task_03/src/task03.cpp
+++ trunk/ii02503/task_03/src/task03.cpp
@@ -6,16 +6,19 @@
 using namespace std;
 
 class Graph {
-    vector<vector<int>> adjList;
+    vector<vector<int>> adjList; // Список смежности
 
 public:
+    // Конструктор графа
     Graph(int size) : adjList(size) {}
 
+    // Добавление ребра в граф
     void addEdge(int u, int v) {
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
 
+    // Печать графа
     void printGraph() const {
         for (int i = 0; i < adjList.size(); ++i) {
             cout << "Node " << i << ": ";
@@ -26,6 +29,7 @@ public:
         }
     }
 
+    // Проверка на наличие эйлерова цикла
     bool hasEulerianCycle() const {
         for (const auto& neighbors : adjList) {
             if (neighbors.size() % 2 != 0) return false;
@@ -33,10 +37,13 @@ public:
         return true;
     }
 
+    // Поиск эйлерова цикла
     vector<int> findEulerianCycle() const {
         if (!hasEulerianCycle()) return {};
-        vector<vector<int>> tempAdj = adjList;
-        vector<int> cycle, stack = { 0 };
+
+        vector<vector<int>> tempAdj = adjList; // Временный список смежности
+        vector<int> cycle; // Вектор для хранения цикла
+        vector<int> stack = { 0 }; // Стек для обхода
 
         while (!stack.empty()) {
             int v = stack.back();
@@ -54,8 +61,9 @@ public:
         return cycle;
     }
 
+    // Поиск гамильтонова цикла
     vector<int> findHamiltonianCycle() const {
-        vector<int> path = { 0 };
+        vector<int> path = { 0 }; // Начинаем с первой вершины
         vector<bool> visited(adjList.size(), false);
         visited[0] = true;
 
@@ -63,12 +71,13 @@ public:
         return {};
     }
 
+    // Построение остовного дерева с использованием BFS
     Graph buildSpanningTree() const {
         Graph tree(adjList.size());
         vector<bool> visited(adjList.size(), false);
         queue<int> q;
 
-        visited[0] = true;
+        visited[0] = true; // Начинаем с первой вершины
         q.push(0);
 
         while (!q.empty()) {
@@ -87,6 +96,7 @@ public:
     }
 
 private:
+    // Рекурсивная функция для поиска гамильтонова цикла
     bool hamiltonianDFS(int node, vector<int>& path, vector<bool>& visited) const {
         if (path.size() == adjList.size()) {
             return find(adjList[node].begin(), adjList[node].end(), path[0]) != adjList[node].end();
@@ -105,6 +115,7 @@ private:
     }
 };
 
+// Инициализация графа с рёбрами
 void initializeGraph(Graph& graph) {
     graph.addEdge(0, 1);
     graph.addEdge(1, 2);
@@ -113,6 +124,7 @@ void initializeGraph(Graph& graph) {
     graph.addEdge(4, 0);
 }
 
+// Поиск и вывод эйлерова цикла
 void findAndPrintEulerianCycle(const Graph& graph) {
     auto eulerCycle = graph.findEulerianCycle();
     if (!eulerCycle.empty()) {
@@ -125,6 +137,7 @@ void findAndPrintEulerianCycle(const Graph& graph) {
     }
 }
 
+// Поиск и вывод гамильтонова цикла
 void findAndPrintHamiltonianCycle(const Graph& graph) {
     auto hamiltonCycle = graph.findHamiltonianCycle();
     if (!hamiltonCycle.empty()) {
@@ -137,24 +150,27 @@ void findAndPrintHamiltonianCycle(const Graph& graph) {
     }
 }
 
+// Построение и вывод остовного дерева
 void buildAndPrintSpanningTree(const Graph& graph) {
     auto spanningTree = graph.buildSpanningTree();
     cout << "Spanning Tree:" << endl;
     spanningTree.printGraph();
 }
 
+// Главная функция
 int main() {
-    Graph graph(5);
+    Graph graph(5); // Создание графа из 5 вершин
 
-    initializeGraph(graph);
+    initializeGraph(graph); // Инициализация графа
 
-    graph.printGraph();
+    cout << "Graph:" << endl;
+    graph.printGraph(); // Печать графа
 
-    findAndPrintEulerianCycle(graph);
+    findAndPrintEulerianCycle(graph); // Поиск и печать эйлерова цикла
 
-    findAndPrintHamiltonianCycle(graph);
+    findAndPrintHamiltonianCycle(graph); // Поиск и печать гамильтонова цикла
 
-    buildAndPrintSpanningTree(graph);
+    buildAndPrintSpanningTree(graph); // Построение и печать остовного дерева
 
     return 0;
 }
