0dfa4a650cfa860ab133aee89df6bfb8b54a4282
curikea <curukovalera7@gmail.com>
2024-12-20 14:33:11 +0300

    [task_03] Add task solution

---
+++ trunk/ii02622/task_03/src/otis3.cpp
@@ -0,0 +1,179 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    int id;
+    vector<int> neighbors;
+};
+
+class Graph {
+public:
+    vector<Node> nodes;
+
+    void addNode(int id) {
+        nodes.push_back({ id, {} });
+    }
+
+    void addEdge(int from, int to) {
+        nodes[from].neighbors.push_back(to);
+        nodes[to].neighbors.push_back(from);
+    }
+
+    void printGraph() const {
+        for (const auto& node : nodes) {
+            cout << "Node " << node.id << ": ";
+            for (int neighbor : node.neighbors) {
+                cout << neighbor << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    vector<int> getEulerianCycle() const {
+        if (!checkEulerian()) {
+            return {};
+        }
+
+        vector<int> cycle;
+        vector<bool> visited(nodes.size(), false);
+        int startNode = 0;
+
+        findEulerianCycleDFS(startNode, visited, cycle);
+
+        return cycle;
+    }
+
+    vector<int> getHamiltonianCycle() {
+        vector<int> cycle(nodes.size(), -1);
+        vector<bool> visited(nodes.size(), false);
+        cycle[0] = 0;
+        visited[0] = true;
+
+        if (findHamiltonian(0, 1, cycle, visited)) {
+            return cycle;
+        }
+        return {};
+    }
+
+    Graph createSpanningTree() const {
+        Graph spanningTree;
+        for (const auto& node : nodes) {
+            spanningTree.addNode(node.id);
+        }
+
+        vector<bool> visited(nodes.size(), false);
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+
+            for (int neighbor : nodes[current].neighbors) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    spanningTree.addEdge(current, neighbor);
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        return spanningTree;
+    }
+
+private:
+    bool checkEulerian() const {
+        for (const auto& node : nodes) {
+            if (node.neighbors.size() % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void findEulerianCycleDFS(int node, vector<bool>& visited, vector<int>& cycle) const {
+        visited[node] = true;
+        cycle.push_back(node);
+
+        for (int neighbor : nodes[node].neighbors) {
+            if (!visited[neighbor]) {
+                findEulerianCycleDFS(neighbor, visited, cycle);
+            }
+        }
+    }
+
+    bool findHamiltonian(int current, int depth, vector<int>& cycle, vector<bool>& visited) {
+        if (depth == nodes.size()) {
+            for (int neighbor : nodes[current].neighbors) {
+                if (neighbor == cycle[0]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        for (int neighbor : nodes[current].neighbors) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                cycle[depth] = neighbor;
+
+                if (findHamiltonian(neighbor, depth + 1, cycle, visited)) {
+                    return true;
+                }
+
+                visited[neighbor] = false;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    Graph graph;
+
+    for (int i = 0; i < 5; ++i) {
+        graph.addNode(i);
+    }
+
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+    graph.addEdge(4, 0);
+
+    cout << "Graph structure:" << endl;
+    graph.printGraph();
+
+    vector<int> eulerianCycle = graph.getEulerianCycle();
+    if (!eulerianCycle.empty()) {
+        cout << "Eulerian cycle: ";
+        for (int node : eulerianCycle) {
+            cout << node << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "No Eulerian cycle found." << endl;
+    }
+
+    vector<int> hamiltonianCycle = graph.getHamiltonianCycle();
+    if (!hamiltonianCycle.empty()) {
+        cout << "Hamiltonian cycle: ";
+        for (int node : hamiltonianCycle) {
+            cout << node << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "No Hamiltonian cycle found." << endl;
+    }
+
+    Graph spanningTree = graph.createSpanningTree();
+    cout << "Spanning tree:" << endl;
+    spanningTree.printGraph();
+
+    return 0;
+}
