64dfdd4b4af3691a9c3bfd99f4d9dd2639fa24ef
Дулько Денис Андреевич <ii002526@g.bstu.by>
2024-12-21 09:33:59 +0300

    uptodate1

--- trunk/ii0230280/lab01/src/1LabOTIS.cpp
+++ trunk/ii0230280/lab01/src/1LabOTIS.cpp
@@ -3,26 +3,24 @@
 
 using namespace std;
 
-void linSim(double coeffA, double coeffB, double initialValue, double inputValue, int steps) {
-    double currentValue = initialValue;
-    double controlValue = inputValue;
+void linearSimulation(double paramA, double paramB, double startValue, double controlInput, int numSteps) {
+    double currentValue = startValue;
 
     cout << "n--- Моделирование линейной системы ---n";
-    for (int step = 0; step < steps; ++step) {
-        double nextValue = coeffA * currentValue + coeffB * controlValue;  
+    for (int step = 0; step < numSteps; ++step) {
+        double nextValue = paramA * currentValue + paramB * controlInput;
         cout << "Шаг " << step + 1 << ": y[" << step + 1 << "] = " << nextValue << endl;
         currentValue = nextValue;
     }
 }
 
-void nonlinsim(double coeffA, double coeffB, double coeffC, double coeffD, double initialValue, double previousValue, double inputValue, int steps) {
-    double currentValue = initialValue;
-    double lastValue = previousValue;
-    double controlValue = inputValue;
+void nonlinearSimulation(double paramA, double paramB, double paramC, double paramD, double startValue, double prevValue, double controlInput, int numSteps) {
+    double currentValue = startValue;
+    double lastValue = prevValue;
 
     cout << "n--- Моделирование нелинейной системы ---n";
-    for (int step = 0; step < steps; ++step) {
-        double nextValue = coeffA * currentValue - coeffB * lastValue * lastValue + coeffC * controlValue + coeffD * sin(controlValue - 1);  // Уравнение нелинейной модели
+    for (int step = 0; step < numSteps; ++step) {
+        double nextValue = paramA * currentValue - paramB * lastValue * lastValue + paramC * controlInput + paramD * sin(controlInput - 1);  // Нелинейная модель
         cout << "Шаг " << step + 1 << ": y[" << step + 1 << "] = " << nextValue << endl;
 
         lastValue = currentValue;
@@ -32,29 +30,29 @@ void nonlinsim(double coeffA, double coeffB, double coeffC, double coeffD, doubl
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    double coeffA, coeffB, coeffC, coeffD;
-    double initialTemp, inputTemp, previousTemp;
+    double paramA, paramB, paramC, paramD;
+    double initialTemperature, controlTemperature, previousTemperature;
     int totalSteps;
 
-    cout << "Введите коэффициент a: ";
-    cin >> coeffA;
-    cout << "Введите коэффициент b: ";
-    cin >> coeffB;
-    cout << "Введите коэффициент c (для нелинейной модели): ";
-    cin >> coeffC;
-    cout << "Введите коэффициент d (для нелинейной модели): ";
-    cin >> coeffD;
-    cout << "Введите начальное значение температуры: ";
-    cin >> initialTemp;
-    cout << "Введите начальное значение предыдущей температуры (для нелинейной модели): ";
-    cin >> previousTemp;
-    cout << "Введите начальное значение управляющего входа: ";
-    cin >> inputTemp;
-    cout << "Введите количество временных шагов: ";
+    cout << " a: ";
+    cin >> paramA;
+    cout << " b: ";
+    cin >> paramB;
+    cout << "c (nonlim): ";
+    cin >> paramC;
+    cout << "d nonlim): ";
+    cin >> paramD;
+    cout << "inT: ";
+    cin >> initialTemperature;
+    cout << "int t ): ";
+    cin >> previousTemperature;
+    cout << "upr op ";
+    cin >> controlTemperature;
+    cout << "steps: ";
     cin >> totalSteps;
 
-    linSim(coeffA, coeffB, initialTemp, inputTemp, totalSteps);
-    nonlinsim(coeffA, coeffB, coeffC, coeffD, initialTemp, previousTemp, inputTemp, totalSteps);
+    linearSimulation(paramA, paramB, initialTemperature, controlTemperature, totalSteps);
+    nonlinearSimulation(paramA, paramB, paramC, paramD, initialTemperature, previousTemperature, controlTemperature, totalSteps);
 
     return 0;
 }
